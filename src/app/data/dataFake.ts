66d6b3bd84682ced23b88b24a1aad8193180acd5
♡Bia♡ <112487610+BiaAS2@users.noreply.github.com>
2024-02-14 23:04:50 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,17 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Novo Homem de Ferro anunciado',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio odio cum architecto fuga. Vel illum dolores laboriosam sapiente officia reprehenderit tenetur quam? Laboriosam dolores quibusdam fugit doloremque necessitatibus optio asperiores.',
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqFTnUZhP5BoG2JeY2fYo2ypFduthqT_VQNQ&usqp=CAU',
+  },
+  {
+    id: '2',
+    title: 'Saiu a nova versão do Angular',
+    description: 'Nova HQ do Homem Aranha é lançada nos EUA',
+    photoCover:
+      'https://images.unsplash.com/photo-1612036782180-6f0b6cd846fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFydmVsfGVufDB8fDB8fHww',
+  },
+];
